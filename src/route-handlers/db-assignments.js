89d4d17709ff1cab9cc4dd9f00510e917e56c70b
Hunter Ledet <hunter.ledet@gmail.com>
2018-02-02 09:28:37 -0600

    [routes] fix assignment bug

--- src/route-handlers/db-assignments.js
+++ src/route-handlers/db-assignments.js
@@ -58,6 +58,7 @@ const deleteAssignment = (info) => {
 const checkAssignment = (sessionId) => {
   return participantDB.searchParticipants(sessionId)
     .then(participants => {
+      console.log(participants, 'participants from checkAssignment');
       return participants;
     })
     .catch(err => console.error(err));
@@ -68,22 +69,29 @@ const specificAssignment = (sessionId, assignmentId) => {
     .then(results => {
       // console.log(results, 'results from check assignment');
       const ids = [];
-      results.forEach(res => ids.push(res.id_participant));
+      // check this res that is pushing in id_participant
+      // console.log(results, 'results from checkAssignment in specificAssignment');
+      results.forEach(res => ids.push(res.participantId));
       return homeworkDB.findHomework(ids, assignmentId)
         .then(res => {
+          // console.log(res, 'res from checkAssignment');
           const format = [];
           res.forEach(el => {
             if (ids.includes(el.id_participant)) {
               format.push({ id_participant: el.id_participant, photoUrl: el.photoUrl });
             }
           });
+          // console.log(format, 'this is format from specificAssignment');
           format.forEach(le => {
+            console.log(le, 'le in db-assignment')
             results.forEach(element => {
-              if (element.id_participant === le.id_participant) {
+              console.log(element, 'el in db-assignment')
+              if (element.participantId === le.id_participant) {
                 element.photoUrl = le.photoUrl;
               }
             });
           });
+          console.log(results, 'results coming out of checkAssignments');
           return results;
         })
         .catch(err => console.error(err));
